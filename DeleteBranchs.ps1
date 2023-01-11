3b71f33743f2bb2254fdb2536d9bfb099c462185
Ricardo <ricardo_x18@hotmail.com>
2023-01-10 23:29:05 -0600

    Iteracion 2 de script

--- DeleteBranchs.ps1
+++ DeleteBranchs.ps1
@@ -6,12 +6,23 @@ if ($branches) {
     # Mostrar lista de ramas que serán eliminadas en una ventana emergente
     $branchesToDelete = $branches | Out-GridView -Title "Selecciona las ramas a eliminar" -PassThru
 
-    # Eliminar ramas seleccionadas
+    # Eliminar ramas remotas seleccionadas
+    foreach ($branch in $branchesToDelete) {
+	#condicional para no eliminar ramas master y release
+        if($branch -ne "* master" -and $branch -ne "release"){
+            git push origin --delete $branch
+            Write-Host "Rama remota $branch eliminada"
+        }else{
+            Write-Host "No es posible eliminar la rama $branch"
+        }
+    }
+
+    # Eliminar ramas locales seleccionadas
     foreach ($branch in $branchesToDelete) {
         #condicional para no eliminar ramas master y release
         if($branch -ne "* master" -and $branch -ne "release"){
             git branch -D $branch
-            Write-Host "Rama $branch eliminada"
+            Write-Host "Rama local $branch eliminada"
         }else{
             Write-Host "No es posible eliminar la rama $branch"
         }
